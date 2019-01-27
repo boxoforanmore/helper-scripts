76aac3a68014b6aa7acaf2597548b4047e56ab6a
Peter Nielson <computergeekpete@gmail.com>
2019-01-26 21:39:38 -0700

    Refined script; automatically creates a venv

--- flask/create_app.sh
+++ flask/create_app.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 
 ##  Prompt for the name of the app
+clear
 read -p "What is the name of your app? => " appname
 echo "Creating basic Flask app with the name $appname"
-
-echo "Navigating into new app folder"
 mkdir $appname
 cd $appname
-
+echo "In directory => $appname"
 echo
 echo
 
-## Choose the local python path
-PYTHON=$(which python3)
 
-## Prompt the user if they want to use a venv
-read -n1 -p "Use a venv? [y,n] => "  mkvenv
-case $mkenv in
-  y|Y)
-    echo
-    echo "Creating venv..."
-    echo
-    source $PYTHON -m venv venv
-    echo
-    echo "Entering venv..."
-    `source ./venv/bin/activate`
-    `pip install flask`
-    `export FLASK_APP=$appname.py` ;;
-  n|N)
-    echo
-    echo "Moving along without setting up a virtual environment..." ;;
-esac
-
+# Create the venv and load it with the flask library
+echo "Creating venv..."
+echo 
+python3 -m venv venv
 echo
+source ./venv/bin/activate
+pip install --upgrade pip
+pip install flask
+export FLASK_APP=$appname.py
+echo
+
+
 ## Create the scaffolding for the app
-echo "Creating the directory environment for the app"
 mkdir app
+
+
+# Main file to run (what the flask run looks at)
 echo "from app import app" >> "$appname.py"
 
+
+# The launcher
 echo "from flask import Flask
 
 app = Flask(__name__)
 
 from app import routes" >> app/__init__.py
 
+
+# The router
 echo "from flask import render_template
 from app import app
 
@@ -51,15 +47,22 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')" >> app/routes.py
+clear
 
+
+# The html files for the main page
 mkdir app/templates
 echo "<h1>Hello, World!</h1>" >> app/templates/index.html
+clear
 
-source venv/bin/activate
+echo "###############################################################"
 echo
+echo "Enter the working directory with => 'cd $appname'"
 echo 
+echo "Launch virtual environment with => 'source ./venv/bin/activate'"
 echo
-echo "Run flask app with: 'flask run'"
-echo 
-echo "To use this command in the future, begin session with: "
+echo "To use the flask command, begin session with: "
 echo "export FLASK_APP=$appname.py"
+echo 
+echo "Run flask  app  with => 'flask run'"
+echo
